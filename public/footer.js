1970d90c6b16822b7bc248442af1273e0c8bf152
marya479 <m.weglinska479@gmail.com>
2024-06-06 10:05:22 +0200

    general

---
+++ public/footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <p>
+        This project was coded by {""}
+        <a
+          href="https://www.linkedin.com/in/marta-lucja-weglinska-761603211/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Marta Lucja
+        </a>
+        {""} and is {""}
+        <a
+          href="/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          on GitHub
+        </a>
+        {""} and {""}
+        <a
+          href="/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </p>
+    </div>
+  );
+}
